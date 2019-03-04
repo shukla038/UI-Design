56e9bdd33a3623ac008a5a12faeacf67425e3f18
Aman Shukla <aman.shukla@rubique.com>
2019-03-04 19:33:12 +0530

    ui development

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,20 +1,15 @@
 import React, { Fragment } from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import NotFoundPage from "../NotFoundPage";
-import Autofill from "../AutoComplete";
 import LandingPage from "../LandingPage";
-import TweetInput from "../TweetInput";
-import QuizComponent from "../Quiz";
 export default function App() {
   return (
     <Fragment>
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={LandingPage} />
-          <Route exact path="/auto-fill-component" component={Autofill} />
-          <Route exact path="/second-question" component={Autofill} />
-          <Route exact path="/third-question" component={TweetInput} />
-          <Route exact path="/quiz/:step" component={QuizComponent} />
+          <Route exact path="/landing-page" component={LandingPage} />
+
           <Route exact path="" component={NotFoundPage} />
         </Switch>
       </BrowserRouter>
