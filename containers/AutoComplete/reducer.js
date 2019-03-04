fa00eaaa89d304c776ce3715e6f34ad4c69455c8
Aman Shukla <aman.shukla@rubique.com>
2019-03-04 20:16:30 +0530

    Updates

--- containers/AutoComplete/reducer.js
+++
@@ -1,9 +0,0 @@
-export default (state = {}, action) => {
-  switch (action.type) {
-    case "FIRST_ACTION": {
-      return { ...state, data: action.payload };
-    }
-    default:
-      return state;
-  }
-};
--- rootreducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux-immutable";
-import { reducer as formReducer } from "redux-form/immutable";
-import simplereducer from "./containers/AutoComplete/reducer";
-export default combineReducers({
-  form: formReducer,
-  simplereducer
-});
